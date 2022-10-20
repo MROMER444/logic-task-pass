7a6f79393eed177297b61d764d06ce03b7f9085a
MROMER444 <omerraad444@gmail.com>
2022-10-20 19:02:26 +0300

    add a prime number to the arguments

--- all_prime.py
+++ all_prime.py
@@ -2,8 +2,8 @@ def check_prime_num(number):
     if number > 1:
         for i in range(2 , int(number)):
             if number % i == 0:
-                print(number, "not prime")
+                print(number, "not prime number")
                 break
         else:
-            print(number, "this is prime")
+            print(number, "prime number")
 check_prime_num(173)
